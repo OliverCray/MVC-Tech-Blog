18d802a15f3e2de299c98a433b1a7de1489d7140
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-08-09 14:13:32 +0100

    Created required models

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize')
+const bcrypt = require('bcrypt')
+const sequelize = require('../config/connection')
+
+class User extends Model {
+  checkPassword(entered_password) {
+    return bcrypt.compareSync(entered_password, this.password)
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    joined_on: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10)
+        return newUserData
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        )
+        return updatedUserData
+      },
+    },
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+)
+
+module.exports = User
