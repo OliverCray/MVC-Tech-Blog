7b01fdeaec586feb9b62aa2e5ddbef85ed018245
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-08-15 14:02:28 +0100

    Created newCommentHandler function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -14,6 +14,35 @@ const logoutHandler = async (event) => {
   }
 }
 
+const newCommentHandler = async (postId, event) => {
+  event.preventDefault()
+
+  const post_id = `${postId}`
+  const body = document.querySelector(`#comment-${postId}`).value.trim()
+
+  if (post_id && body !== '') {
+    try {
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ post_id, body }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if (response.ok) {
+        document.location.replace(`/post/${postId}`)
+      } else {
+        const responseData = await response.json()
+        alert(responseData.message || 'Failed to create comment')
+      }
+    } catch (error) {
+      console.error('An error occurred:', error)
+      alert('An error occurred. Please try again.')
+    }
+  }
+}
+
 document
   .querySelector('#logout-button')
   .addEventListener('click', logoutHandler)
