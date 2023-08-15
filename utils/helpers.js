55e8650c3f2abe683e2bd65b8f86cde26d5c5066
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-08-15 15:59:06 +0100

    Updated isCurrentPage helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,8 +2,8 @@ const formatDate = (date) => {
   return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 }
 
-const isCurrentPage = (currentPage, page) => {
-  return currentPage === page
+const isCurrentPage = (pageNumber, currentPage) => {
+  return pageNumber === currentPage
 }
 
 module.exports = { formatDate, isCurrentPage }
