d243e1160e2c8b38c4923aea76a7ff76ef7f85d1
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-08-18 02:29:24 +0100

    darkMode state is now stored in local storage

--- public/scripts/toggleDarkMode.js
+++ public/scripts/toggleDarkMode.js
@@ -1,8 +1,16 @@
 const toggleDarkModeButton = document.querySelector('#dark-mode-toggle')
+const darkModeEnabled = localStorage.getItem('darkModeEnabled')
+
+if (darkModeEnabled === 'true') {
+  document.documentElement.classList.add('dark')
+  document.querySelector('#sun-icon').removeAttribute('hidden')
+  document.querySelector('#moon-icon').setAttribute('hidden', '')
+}
 
 toggleDarkModeButton.addEventListener('click', (event) => {
   event.preventDefault()
-  document.documentElement.classList.toggle('dark')
-  document.querySelector('#sun-icon').toggleAttribute('hidden')
-  document.querySelector('#moon-icon').toggleAttribute('hidden')
+  const isDarkMode = document.documentElement.classList.toggle('dark')
+  document.querySelector('#sun-icon').toggleAttribute('hidden', !isDarkMode)
+  document.querySelector('#moon-icon').toggleAttribute('hidden', isDarkMode)
+  localStorage.setItem('darkModeEnabled', isDarkMode)
 })
